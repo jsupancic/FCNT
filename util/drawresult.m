d5949bbed1edb0be7e34836541c7c71b0ab205f3
James Supancic III <arrummzen@gmail.com>
2016-04-30 14:32:20 -0700

    update

--- util/drawresult.m
+++ util/drawresult.m
@@ -1,11 +1,11 @@
-function k_bb = drawresult(fno, frame, sz, mat_param)
-figure(1); clf;
-set(gcf,'DoubleBuffer','on','MenuBar','none');
+function k_bb = drawresult(fno, frame, sz, mat_param, color)
+sfigure(1); clf;
+%set(gcf,'DoubleBuffer','on','MenuBar','none');
 colormap('gray');
 axes('position', [0 0 1 1])
 imagesc(frame, [0,1]); hold on;
 text(5, 18, num2str(fno), 'Color','y', 'FontWeight','bold', 'FontSize',18);
-k_bb = drawbox(sz(1:2), mat_param, 'Color','r', 'LineWidth',2.5);
+k_bb = drawbox(sz(1:2), mat_param, 'Color',color, 'LineWidth',2.5);
 axis off; hold off;
 drawnow;
 end
